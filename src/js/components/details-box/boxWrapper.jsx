4121e962218d0048f5cfc427955703b1847a4c87
Diego <diegozaka@gmail.com>
2016-11-04 18:05:41 -0300

    Added modals to detail boxes

--- src/js/components/details-box/boxWrapper.jsx
+++ src/js/components/details-box/boxWrapper.jsx
@@ -2,7 +2,21 @@
 import { PROCESS_STATE_COMPLETED, PROCESS_STATE_FAILED, PROCESS_STATE_RUNNING, PROCESS_STATE_PENDING } from 'js/consts/processStates';
 import React, { Component } from 'react';
 import classNames from 'classnames';
-import { Link } from 'react-router';
+import Modal from 'react-modal';
+
+const customModalStyles = {
+  content: {
+    top         : '50%',
+    left        : '50%',
+    right       : 'auto',
+    bottom      : 'auto',
+    marginRight : '-50%',
+    paddingTop  : '30px',
+    transform   : 'translate(-50%, -50%)',
+    width       : '350px',
+    height      : '320px'
+  }
+};
 
 type Props = {
   title : string,
@@ -12,6 +26,21 @@ type Props = {
 
 export default class BoxWrapper extends Component {
   props: Props
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = { modalIsOpen: false };
+  }
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = (e) => {
+    e.preventDefault();
+    this.setState({ modalIsOpen: false });
+  }
 
   boxCssClasses(state: string = PROCESS_STATE_PENDING) {
     return classNames({
@@ -44,10 +73,23 @@ export default class BoxWrapper extends Component {
       this.props.state !== PROCESS_STATE_PENDING && this.props.children || this.renderPendingBox();
 
     return (
-      <div className={this.boxCssClasses(this.props.state)}>
+      <div className={this.boxCssClasses(this.props.state)} onClick={this.openModal}>
         <p className="details-box__title">{this.props.title}</p>
         {content}
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel="Example Modal"
+          style={customModalStyles}
+        >
+          <h2>{this.props.title}</h2>
+          {content}
+          <a className="close-button" href="#" onClick={this.closeModal}>
+            <span>×</span>
+          </a>
+        </Modal>
       </div>
+
     );
   }
 }
